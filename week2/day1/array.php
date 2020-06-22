4ed234f48960cb12aa44eb1a8dda5f983f08cf08
lgarin211 <lagustinus211@gmail.com>
2020-06-22 14:55:13 +0700

    data masuk

---
+++ week2/day1/array.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+<body>
+    <h1>Berlatih Array</h1>
+        
+    <?php 
+        echo "<h3> Soal 1 </h3>";
+        /* 
+            SOAL NO 1
+            Kelompokkan nama-nama di bawah ini ke dalam Array.
+            Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
+            Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
+        */
+        $kids; // Lengkapi di sini
+        $adults; 
+        echo "<h3> Soal 2</h3>";
+        /* 
+            SOAL NO 2
+            Tunjukkan panjang Array di Soal No 1 dan tampilkan isi dari masing-masing Array.
+        */
+        echo "Cast Stranger Things: ";
+        echo "<br>";
+        echo "Total Kids: " ; // Berapa panjang array kids
+        echo "<br>";
+        echo "<ol>"; 
+        echo "<li> $kids[0] </li>";
+        // Lanjutkan
+
+        echo "</ol>";
+        
+        echo "Total Adults: " ;// Berapa panjang array adults
+        echo "<br>";
+        echo "<ol>";
+        echo "<li> $adults[0] </li>";
+        // Lanjutkan
+
+        echo "</ol>";
+
+        /*
+            SOAL No 3
+            Susun data-data berikut ke dalam bentuk Asosiatif Array (Array Multidimensi)
+            
+            Name: "Will Byers"
+            Age: 12,
+            Aliases: "Will the Wise"
+            Status: "Alive"
+
+            Name: "Mike Wheeler"
+            Age: 12,
+            Aliases: "Dungeon Master"
+            Status: "Alive"
+
+            Name: "Jim Hopper"
+            Age: 43,
+            Aliases: "Chief Hopper"
+            Status: "Deceased"
+
+            Name: "Eleven"
+            Age: 12,
+            Aliases: "El"
+            Status: "Alive"
+            
+        */
+        
+    ?>
+</body>
+</html>
---
+++ week2/day1/string.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String PHP</title>
+</head>
+<body>
+    <h1>Berlatih String PHP</h1>
+    <?php   
+        echo "<h3> Soal No 1</h3>";
+        /* 
+            SOAL NO 1
+            Tunjukan dengan menggunakan echo berapa panjang dari string yang diberikan berikut! Tunjukkan juga jumlah kata di dalam kalimat tersebut! 
+
+            Contoh: 
+            $string = "PHP is never old";
+            Output:
+            Panjang string: 16, 
+            Jumlah kata: 4 
+        */
+
+        $first_sentence = "Hello PHP!" ; // Panjang string 10, jumlah kata: 2
+        $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
+        
+        echo "<h3> Soal No 2</h3>";
+        /* 
+            SOAL NO 2
+            Mengambil kata pada string dan karakter-karakter yang ada di dalamnya. 
+            
+            
+        */
+        $string2 = "I love PHP";
+        
+        echo "<label>String: </label> \"$string2\" <br>";
+        echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
+        // Lanjutkan di bawah ini
+        echo "Kata kedua: " ;
+        echo "<br> Kata Ketiga: " ;
+
+        echo "<h3> Soal No 3 </h3>";
+        /*
+            SOAL NO 3
+            Mengubah karakter atau kata yang ada di dalam sebuah string.
+        */
+        $string3 = "PHP is old but sexy!";
+        echo "String: \"$string3\" "; 
+        // OUTPUT : "PHP is old but awesome"
+
+    ?>
+</body>
+</html>
